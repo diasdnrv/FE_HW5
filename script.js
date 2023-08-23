cb513c006485e6ee63d80e5c301e7746a2293801
diasdnrv <137884547+diasdnrv@users.noreply.github.com>
2023-08-23 21:18:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Написать программу, которая получает два числа и выводит наименьшее
+
+// const firstNum = +prompt("Write a first number")
+// const secondNum = +prompt("Write a second number")
+// if (firstNum < secondNum) {
+//   console.log(firstNum)
+// } else {
+//   console.log(secondNum)
+// }
+
+// Написать программу, которая считывает через prompt число и выводит одно из следующих сообщений: ‘Число меньше 100’, ‘Число больше 100’ или ‘Число равно 100’
+
+// const num = +prompt("Write a number")
+// if (num < 100) {
+//   console.log("Число меньше 100");
+// } else if (num > 100) {
+//   console.log("Число больше 100");
+// } else {
+//   console.log("Число равно 100");
+// }
+
+// Написать программу, которая запрашивает у пользователя его имя и возраст (в годах) и выводит в консоль сообщение ‘Hello, <name>’, если пользователь совершеннолетний, или ‘Hi, <name>’, если пользователь несовершеннолетний.
+
+// let firstName = prompt("Write your name")
+// let age = prompt("Write your age")
+
+// if (age >= 18) {
+//     console.log(`Hello, ${firstName}`)
+// } else {
+//     console.log(`Hi, ${firstName}`);
+// }
